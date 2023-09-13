d4619ec64be756722e9e5c9af184360c357458c0
vikramr844 <144251870+vikramr844@users.noreply.github.com>
2023-09-13 17:13:40 +0530

    main.py

--- challenge-unit 2/main.py
+++ challenge-unit 2/main.py
@@ -1,8 +1,46 @@
-# Get the year as input from the user
-year = int(input("Enter a year: "))
+'''Implement a class called BankAccount that represents a bank account. 
+The class should have private attributes for account number, account holder name,
+and account balance. Include methods to deposit money, withdraw money, and display the account
+balance. Ensure that the account balance cannot be accessed directly from outside the class. 
+Write a program to create an instance of the BankAccount class and test the deposit and withdrawal 
+functionality. '''
 
-# Check if it's a leap year using if-elif-else statements
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"{year} is a leap year.")
-else:
-    print(f"{year} is not a leap year.")
+
+
+
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive amount.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.__account_balance >= amount:
+                self.__account_balance -= amount
+                print(f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}")
+            else:
+                print("Insufficient funds for withdrawal.")
+        else:
+            print("Invalid withdrawal amount. Please enter a positive amount.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name}: ₹{self.__account_balance}")
+
+# Creating an instance of the BankAccount class
+account = BankAccount("123456789", "VIKRAM", 1000)
+
+# Testing deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500)
+account.withdraw(200)
+account.display_balance()
+account.withdraw(1500)
+
